c39269612769dec561e40ea41dd47e67c33b0d18
Johan Backman <johan@brkt.com>
2017-12-17 13:12:59 -0800

    Explicit rate change indicator

--- src/constants.py
+++ src/constants.py
@@ -2,7 +2,8 @@
 """Global constants."""
 # -*- coding: utf-8 -*-
 
-DIR_TO_CHAR = {'NONE': '⇼', 'DoubleUp': '⇈', 'SingleUp': '↑',
-               'FortyFiveUp': '↗', 'Flat': '→', 'FortyFiveDown': '↘',
-               'SingleDown': '↓', 'DoubleDown': '⇊',
-               'NOT COMPUTABLE': '-', 'RATE OUT OF RANGE': '⇕'}
+DIR_TO_CHAR = {'NONE': '<->', 'DoubleUp': 'Up Up', 'SingleUp': 'Up',
+               'FortyFiveUp': 'Slight up', 'Flat': 'Flat',
+               'FortyFiveDown': 'Slight down', 'SingleDown': 'Down',
+               'DoubleDown': 'Down Down', 'NOT COMPUTABLE': '-',
+               'RATE OUT OF RANGE': 'OUT OF RANGE'}
